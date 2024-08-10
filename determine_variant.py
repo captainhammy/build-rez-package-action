b14da6805219405b2f9fbaa884e4d6081f181c7a
Graham Thompson <captainhammy@gmail.com>
2024-08-10 11:34:45 -0700

    add debug info for cwd

--- determine_variant.py
+++ determine_variant.py
@@ -46,12 +46,13 @@ def requests_match(request1: PackageRequest, request2: PackageRequest) -> bool:
 
 
 def find_matching_variant(request_str: str) -> int:
+    raise RuntimeError(pathlib.Path.cwd())
+
     if not request_str:
         return -1
 
     request = PackageRequest(request_str)
 
-    raise RuntimeError(pathlib.Path.cwd())
     package = get_package(pathlib.Path.cwd())
     # package = get_package(pathlib.Path("/home/graham/src/houdini-toolbox-inlinecpp"))
 
