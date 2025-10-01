b5d2e656e06606195f88e8d67f9e4cb0da3115a5
Graham Thompson <captainhammy@gmail.com>
2025-10-01 08:24:38 -0700

    4 support building a package from git (#5)
    feat: add a `from_git` workflow parameter to check out a git repo and use that location as the package root for building

---
+++ get_package_root.py
@@ -0,0 +1,83 @@
+"""Program to determine the actual package root."""
+
+# Future
+from __future__ import annotations
+
+# Standard Library
+import argparse
+import os
+import pathlib
+import shutil
+from urllib.parse import urlparse
+
+# Third Party
+from git import Repo
+
+
+def build_parser() -> argparse.ArgumentParser:
+    """Build the program's argument parser.
+
+    Returns:
+        The argument parser.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("root_parameter")
+    parser.add_argument("from_git")
+    parser.add_argument("--tag", default=None)
+
+    return parser
+
+
+def checkout_git_repo(git_url: str, target_dir: pathlib.Path, tag_name: str | None = None) -> None:
+    """Checkout the git repo at the given URL.
+
+    Args:
+        git_url: The girt repo URL.
+        target_dir: The directory to clone the repo to.
+        tag_name: Optional tag name.
+    """
+    if target_dir.exists():
+        shutil.rmtree(target_dir)
+
+    repo = Repo.clone_from(git_url, target_dir)
+
+    if tag_name is not None:
+        repo.head.reference = repo.tags[tag_name].commit
+        repo.head.reset(index=True, working_tree=True)
+
+
+def write_result(package_root: str) -> None:
+    """Write the package name to the outputs file.
+
+    Args:
+        package_root: The package variant.
+    """
+    output_path = pathlib.Path(os.environ["GITHUB_OUTPUT"])
+
+    with output_path.open("a", encoding="utf-8") as fp:
+        fp.write(f"package_root={package_root}\n")
+
+
+def main() -> None:
+    """The program."""
+    parser = build_parser()
+    args = parser.parse_args()
+    root_parameter = args.root_parameter
+    url = args.from_git
+
+    if url:
+        parsed_url = urlparse(url)
+        repo_name = os.path.splitext(os.path.basename(parsed_url.path))[0]
+
+        git_path = pathlib.Path(os.environ["RUNNER_TEMP"]) / repo_name
+        checkout_git_repo(url, git_path, tag_name=args.tag)
+
+        write_result(git_path.as_posix())
+
+    else:
+        write_result(root_parameter)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/python/test_get_package_root.py
@@ -0,0 +1,100 @@
+"""Test the get_package_root program."""
+
+# Standard Library
+import pathlib
+from argparse import Namespace
+
+# Third Party
+import git
+import pytest
+
+# build-rez-packages-action
+import get_package_root
+
+
+def test_build_parser():
+    """Test get_package_root.build_parser()."""
+    parser = get_package_root.build_parser()
+
+    expected = [action.dest for action in parser._actions[1:]]
+
+    assert expected == ["root_parameter", "from_git", "tag"]
+
+
+@pytest.mark.parametrize(
+    "tag,exists",
+    (
+        (None, False),
+        ("1.2.3", True),
+    ),
+)
+def test_checkout_git_repo(mocker, tag, exists):
+    """Test get_package_info.checkout_git_repo()."""
+    mock_rmtree = mocker.patch("shutil.rmtree")
+
+    mock_repo = mocker.MagicMock(spec=git.Repo)
+    mock_clone = mocker.patch.object(get_package_root.Repo, "clone_from", return_value=mock_repo)
+
+    mock_url = mocker.MagicMock(spec=str)
+    mock_path = mocker.MagicMock(spec=pathlib.Path)
+    mock_path.exists.return_value = exists
+
+    get_package_root.checkout_git_repo(mock_url, mock_path, tag_name=tag)
+
+    mock_clone.assert_called_once_with(mock_url, mock_path)
+
+    assert mock_rmtree.call_count == int(exists)
+
+    if tag is not None:
+        assert mock_repo.head.reference == mock_repo.tags[tag].commit
+        mock_repo.head.reset.assert_called_with(index=True, working_tree=True)
+
+
+@pytest.mark.parametrize(
+    "git_url, git_tag",
+    (
+        ("", None),
+        ("https://github.com/captainhammy/houdini-core-tools.git", None),
+        ("https://github.com/captainhammy/houdini-core-tools.git", "1.2.3"),
+    ),
+)
+def test_main(monkeypatch, tmp_path, mocker, git_url, git_tag):
+    """Test get_package_root.main()."""
+    monkeypatch.setenv("RUNNER_TEMP", tmp_path.as_posix())
+
+    mock_checkout = mocker.patch("get_package_root.checkout_git_repo")
+
+    expected_path = tmp_path / "houdini-core-tools"
+
+    mock_namespace = mocker.MagicMock(spec=Namespace)
+    mock_namespace.root_parameter = "."
+    mock_namespace.from_git = git_url
+    mock_namespace.tag = git_tag
+
+    mock_parser = mocker.patch("get_package_root.argparse.ArgumentParser")
+    mock_parser.return_value.parse_args.return_value = mock_namespace
+
+    mock_write = mocker.patch("get_package_root.write_result")
+
+    get_package_root.main()
+
+    if git_url:
+        mock_checkout.assert_called_with(git_url, expected_path, tag_name=git_tag)
+        mock_write.assert_called_with(expected_path.as_posix())
+
+    else:
+        mock_write.assert_called_with(".")
+
+
+def test_write_result(monkeypatch, tmp_path):
+    """Test get_package_root.write_result()."""
+    output_file = tmp_path / "test_output.txt"
+
+    monkeypatch.setenv("GITHUB_OUTPUT", output_file.as_posix())
+
+    expected = "/path/to/package_root/"
+
+    get_package_root.write_result(expected)
+
+    with output_file.open() as handle:
+        assert handle.read() == f"package_root={expected}\n"
